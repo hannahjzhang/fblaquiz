f8523be1b32cfd771cfcd32c32ad54f3ccee21dd
Hannah Zhang <hannahjzhang@gmail.com>
2020-12-27 11:05:10 -0800

    Cleanup.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// what shows up on the main page
+import React from "react";
+import "./App.css";
+// imports the questions from quiz.js with proper instructions and displays it
+import { Questions } from "./Quiz";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Questions />
     </div>
   );
 }
