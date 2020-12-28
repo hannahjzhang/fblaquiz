e88a5ee1335741369320c65addf86888e420ba74
Hannah Zhang <hannahjzhang@gmail.com>
2020-12-28 11:31:40 -0800

    adding quiz questions

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,77 +3,123 @@ import "./App.css";
 // import the quiz question data from data.js and determines what to do with it
 import { quizData } from "./data";
 
-// exports Questions to app.js to be displayed
-export const Questions = () => {
-  return (
-    <>
-      <HomePageHeader />
-      <div className="question-container">
-        {quizData.map((data, key) => {
-          return (
-            <div key={key}>
-              <Question
-                key={key}
-                question={data.question}
-                options={data.options}
-              />
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
-};
-
+// header for the page
 const HomePageHeader = () => {
   return (
     <header className="header">
-      <h2>FBLA Quiz</h2>
-      <p>You will be given a quiz with 5 random questions regarding FBLA.
-        Answer the questions to the best of your ability and then see your score!</p>
+      <h1>Introduction to FBLA Quiz</h1>
     </header>
   );
 };
 
-const Question = ({ question, options }) => {
-  if (!question) return <div />;
+// for the submit button
+const Submission = () => {
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td>
-            <h5>{question}</h5>
-          </td>
-          <td>
-            <h5>{options}</h5>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="button">
+      Submit
+    </div>
   );
 };
-/*
-export const Questions = () => {
+
+const Footer = () => {
+  return  (
+    <div className="footer">
+      <p> Made with ❤️ by Hannah Zhang </p>
+    </div>
+  )
+}
+
+// function called for true/false Questions
+function TF(props) {
+  return (
+    <div className="question-container">
+      {/* display the prompt */}
+      <div key="question" className="prompt-container">
+        {props.num}) {props.data.question}
+      </div>
+      {/* display the answer */}
+      <div key="answer" class="answer-container">
+        <input type="radio" name={props.num} className="radio"/> <label> {props.data.option1}</label> <br />
+        <input type="radio" name={props.num} className="radio"/> <label> {props.data.option2}</label>
+      </div>
+    </div>
+  );
+}
+
+function Multiple(props) {
+  return (
+    <div className="question-container">
+      <div key="question" className="prompt-container">
+        {props.num}) {props.data.question}
+      </div>
+      <div key="answer" className="answer-container">
+        <input type="radio" name={props.num} className="radio"/> <label for="option">{props.data.option1}</label> <br />
+        <input type="radio" name={props.num} className="radio"/> <label for="option">{props.data.option2}</label> <br />
+        <input type="radio" name={props.num} className="radio"/> <label for="option">{props.data.option3}</label> <br />
+        <input type="radio" name={props.num} className="radio"/> <label for="option">{props.data.option4}</label> <br />
+      </div>
+    </div>
+  );
+}
+
+// function called whenever a question of type fillintheblank is called
+function Blank(props) {
+  var str = props.data.question;
+  var index = str.indexOf("$BLANK");
   return (
-    <>
-      <div className="question-container">
-        {quizData.map((data, key) => {
-          if (data.type === "truefalse") {
-            return (
-              <div key={key}>
-                {data.question} + "," + {data.options}
-              </div>
-            );
-          } else {
-            return (
-              <div key={key}>
-                {data.type}
-              </div>
-            );
-          }
-        })}
-      </div>
-    </>
+    <div className="question-container">
+      <div key="question" className="prompt-container">
+        {props.num}) {str.substring(0, index)}
+        <input type="text" className="blank"/>
+        {str.substring(index+6)}
+      </div>
+    </div>
   );
+}
+
+// function called whenever a question of type dropdown is called
+function Dropdown(props) {
+  var str = props.data.question;
+  var idx = str.indexOf("$CHOICES");
+  return (
+    <div className="question-container">
+      <div key="question" className="prompt-container">
+        {props.num}) {str.substring(0, idx)}
+          <select name="Dropdown" className="dropdown">
+            {props.data.options.map((val,key)=>{
+              return <option key={key} value={val}> {val} </option>;
+            })}
+          </select> {str.substring(idx+8)}
+      </div>
+    </div>
+  );
+}
+
+// calls functions based on question type and exports Questions to app.js to be displayed
+export const Questions = () => {
+  return (
+    <>
+      <HomePageHeader />
+      <div className="page-container">
+        {quizData.map((data, key) => {
+          if (data.type === "truefalse") {
+            return (
+              <TF data={data} num={key+1} />
+            );
+          }
+          else if (data.type === "mcq") {
+            return (<Multiple data={data} num={key+1} />);
+          }
+          else if (data.type === "blank"){
+            return (<Blank data={data} num={key+1} />);
+          }
+          else {
+            return (<Dropdown data={data} num={key+1} />);
+          }
+        })}
+      </div>
+      <Submission />
+      <Footer />
+    </>
+  );
 };
-*/
--- src/data.js
+++ src/data.js
@@ -4,35 +4,37 @@ export const quizData = [
   {
     type: "truefalse",
     question: "Dr. Edward Miller was the first director of FBLA-PBL.",
-    options: [
-      "True",
-      "False"
-    ],
+    option1: "true", option2: "false",
     answer: "true"
   },
+  {
+    type: "truefalse",
+    question: "The three types of events are individual, duo, and team.",
+    option1: "true", option2: "false",
+    answer: "false"
+  },
   {
     type: "mcq",
-    question: "What are the official colors of FBLA?...",
-    options: [
-      "Blue and gold",
-      "Red and white",
-      "Blue and white",
-      "Red and blue"
-    ],
-    answer: "Blue and gold"
+    question: "What are the official colors of FBLA?",
+    option1: "blue and gold", option2: "red and white", option3: "red and blue", option4: "blue and gold",
+    answer: "blue and gold"
+  },
+  {
+    type: "mcq",
+    question: "Key words found in the FBLA goals include:",
+    option1: "confidence, understanding, efficient, scholarship", option2: "seek, sincere, abide, initiative",
+    option3: "promise, aims, qualities, active", option4: "manner, directors, regulations, cooperations",
+    answer: "confidence, understanding, efficient, scholarship"
   },
   {
     type: "dropdown",
-    question: "The FBLA creed contains how many stanzas?",
-    options: [
-      "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
-    ],
-    answer: "7"
+    question: "The FBLA creed contains $CHOICES stanza(s)?",
+    options: ["  ", "one", "two", "three", "four", "five", "six", "seven", "eight"],
+    answer: "seven"
   },
   {
     type: "blank",
-    question: "The first FBLA chapter was chartered in $blank.",
-    options: "blank",
+    question: "The first FBLA chapter was chartered in $BLANK.",
     answer: "tennessee"
   },
 ];
